b751ba40cc4d9aab1a545a180bc5cae613b389aa
tugcecorekci <tugcecorekcioglu@gmail.com>
2022-02-04 18:25:27 +0100

    Added authorization.

--- models/campground.js
+++ models/campground.js
@@ -8,13 +8,17 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
     ]
-});
+})
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '61f961935d965493bcdb8bc8', //username:tugce
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
